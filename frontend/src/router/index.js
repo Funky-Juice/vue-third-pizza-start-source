f05a366e89e73508db58ac26fd6b43e864c1984a
Sergey Inshakov <isa86box@gmail.com>
2024-03-10 19:22:06 +0300

    Создание navigation guards: перед реализацией navigation guards нужно создать механизм их регистрации и вызова

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { routes } from "./routes.js";
+import { routes } from "./routes";
+import { middlewarePipeline } from "@/middlewares/middlewarePipeline";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+export const router = createRouter({
+  history: createWebHistory(),
   routes,
 });
 
-export default router;
+middlewarePipeline(router);
